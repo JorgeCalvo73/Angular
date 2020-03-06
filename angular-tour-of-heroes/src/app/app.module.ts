562a8a61331dcf69135cb2f419aef7fb403f4338
Jorge Calvo <calvoblancojorge@gmail.com>
2020-03-06 19:56:01 +0100

    Tour of heroes created

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -3,14 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    ProductListComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
---
+++ my-app/src/app/product-list/product-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { products } from '../products';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+
+  products = products;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  add() {
+    window.alert('Product added to the cart!');
+  }
+
+}
